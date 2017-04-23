b51f088264c2905d1cfc12a1fdb353fb1ae0b45a
kiemlicz <stanislaw.dev@gmail.com>
2017-04-23 12:53:40 +0200

    Tests for envoy  - fixed improper show_sls (inherited was not taken into account properly)  - added different pillar data for tests

--- .travis/envoy_test.py
+++ .travis/envoy_test.py
@@ -8,13 +8,18 @@ class ParametrizedTestCase(unittest.TestCase):
         inherit from this class.
     """
 
-    def __init__(self, methodName='runTest', saltenv=None, location=None):
+    def __init__(self, methodName='runTest', saltenv=None, pillarenv=None, saltenv_location=None,
+                 pillarenv_location=None):
         super(ParametrizedTestCase, self).__init__(methodName)
         self.saltenv = saltenv
-        self.location = location
+        self.saltenv_location = saltenv_location
+        self.pillarenv = pillarenv
+        self.pillarenv_location = pillarenv_location
 
     @staticmethod
-    def parametrize(testcase_klass, saltenv=None, location=None):
+    def parametrize(testcase_klass, saltenv=None, pillarenv=None,
+                    saltenv_location='/srv/salt',
+                    pillarenv_location='/srv/pillar/'):
         """ Create a suite containing all tests taken from the given
             subclass, passing them the parameter 'param'.
         """
@@ -22,26 +27,35 @@ class ParametrizedTestCase(unittest.TestCase):
         testnames = testloader.getTestCaseNames(testcase_klass)
         suite = unittest.TestSuite()
         for name in testnames:
-            suite.addTest(testcase_klass(name, saltenv=saltenv, location=location))
+            suite.addTest(testcase_klass(name, saltenv=saltenv, pillarenv=pillarenv, saltenv_location=saltenv_location,
+                                         pillarenv_location=pillarenv_location))
         return suite
 
 
 class AmbassadorTest(ParametrizedTestCase):
     def test_states(self):
-        self.assertTrue(os.path.isdir(os.path.join(self.location, self.saltenv)),
-                        msg="not found: {}".format(os.path.join(self.location, self.saltenv)))
+        self.assertTrue(os.path.isdir(os.path.join(self.saltenv_location, self.saltenv)),
+                        msg="salt states not found: {}".format(os.path.join(self.saltenv_location, self.saltenv)))
+        self.assertTrue(os.path.isdir(os.path.join(self.pillarenv_location, self.pillarenv)),
+                        msg="pillar data not found: {}".format(os.path.join(self.pillarenv_location, self.pillarenv)))
         caller = salt.client.Caller()
-        tops = caller.cmd("state.show_top", self.saltenv)
+        tops = caller.cmd("state.show_top")
         for env, states in tops.iteritems():
             for state in states:
-                result = caller.cmd("state.show_sls", state, env)
+                result = caller.cmd("state.show_sls", state, saltenv=env, pillarenv=self.pillarenv)
                 self.assertTrue(isinstance(result, dict),
-                                msg="rendering of: {} (env={}), failed with: {}".format(state, env, result))
+                                msg="rendering of: {} (saltenv={}, pillarenv={}), failed with: {}".format(state,
+                                                                                                          env,
+                                                                                                          self.pillarenv,
+                                                                                                          result))
 
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    suite.addTest(ParametrizedTestCase.parametrize(AmbassadorTest, saltenv='base', location='/srv/salt'))
-    suite.addTest(ParametrizedTestCase.parametrize(AmbassadorTest, saltenv='gui', location='/srv/salt'))
-    suite.addTest(ParametrizedTestCase.parametrize(AmbassadorTest, saltenv='dev', location='/srv/salt'))
+    suite.addTest(ParametrizedTestCase.parametrize(AmbassadorTest, saltenv='base', pillarenv='empty'))
+    suite.addTest(ParametrizedTestCase.parametrize(AmbassadorTest, saltenv='gui', pillarenv='empty'))
+    suite.addTest(ParametrizedTestCase.parametrize(AmbassadorTest, saltenv='dev', pillarenv='empty'))
+    suite.addTest(ParametrizedTestCase.parametrize(AmbassadorTest, saltenv='base', pillarenv='one_user'))
+    suite.addTest(ParametrizedTestCase.parametrize(AmbassadorTest, saltenv='gui', pillarenv='one_user'))
+    suite.addTest(ParametrizedTestCase.parametrize(AmbassadorTest, saltenv='dev', pillarenv='one_user'))
     unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ .travis/pillar/one_user/top.sls
@@ -0,0 +1,3 @@
+one_user:
+  '*':
+    - user
