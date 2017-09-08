8ec5173aa0bbace9718f8a0244ed7fbd26685498
kiemlicz <stanislaw.dev@gmail.com>
2017-09-08 13:21:57 +0200

    Simple debug

--- .travis/entrypoint_run.sh
+++ .travis/entrypoint_run.sh
@@ -7,4 +7,5 @@ tail -f /var/log/salt/minion &
 service salt-minion restart
 #debug may be useful for travis
 salt-call --local state.highstate saltenv=base pillarenv=one_user -l debug | tee output
+echo "salt-call finished, scanning output"
 cat output | awk '/^Failed:/ {if($2 != "0") exit 1}'
