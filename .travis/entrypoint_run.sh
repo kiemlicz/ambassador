37df10d9fd86ed80d59f3060b22959bc9554b164
kiemlicz <stanislaw.dev@gmail.com>
2017-09-20 16:12:03 +0200

    Making samba states pass tests quick workaround for systemd in service in docker containers

--- .travis/entrypoint_run.sh
+++ .travis/entrypoint_run.sh
@@ -4,6 +4,9 @@ rm /var/log/salt/minion
 #so that docker logs will display it
 ln -sf /proc/$$/fd/1 /var/log/salt/minion
 tail -f /var/log/salt/minion &
+# workaround for salt's service state
+echo -e '#!/bin/bash\necho "N 5"' > /sbin/runlevel
+chmod 775 /sbin/runlevel
 service salt-minion restart
 #debug may be useful for travis
 salt-call --local state.highstate saltenv=base pillarenv=one_user -l debug | tee output
