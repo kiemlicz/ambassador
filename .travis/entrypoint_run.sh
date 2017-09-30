4a3350067f736d30ef1294a3347bba74eaa53165
kiemlicz <stanislaw.dev@gmail.com>
2017-09-30 13:06:39 +0200

    Tunable log level for salt better error indication of salt-call (exit code = 3)

--- .travis/entrypoint_run.sh
+++ .travis/entrypoint_run.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+echo "starting (args = $@)"
 rm /var/log/salt/minion
 #so that docker logs will display it
 ln -sf /proc/$$/fd/1 /var/log/salt/minion
 tail -f /var/log/salt/minion &
+
 # workaround for salt's service state
 echo -e '#!/bin/bash\necho "N 5"' > /sbin/runlevel
 chmod 775 /sbin/runlevel
+
 service salt-minion restart
-#debug may be useful for travis
-salt-call --local state.highstate saltenv=base pillarenv=one_user -l debug | tee output
+#debug may be useful for travis (-l debug)
+salt-call --local state.highstate saltenv=base pillarenv=one_user -l ${1-info} | tee output
+#travis sometimes cannot handle properly this much of logs
+sleep 2
 echo "salt-call finished, scanning output"
-cat output | awk '/^Failed:/ {if($2 != "0") exit 1}'
+cat output | awk '/^Failed:/ {if($2 != "0") exit 3}'
