3a7e0a8d754e15db0afaa30a9877c10532676486
kiemlicz <stanislaw.dev@gmail.com>
2017-11-05 15:04:29 +0100

    Removing google-auth manual pip installation from tests Removing in-entrypoint logs workaround, using in-Dockerfile /dev/stdout symlink Enabling ubuntu-artful

--- .travis/entrypoint_run.sh
+++ .travis/entrypoint_run.sh
@@ -4,11 +4,6 @@ echo "OS version:"
 cat /etc/debian_version
 echo "starting (args = $@)"
 
-rm /var/log/salt/minion
-#so that docker logs will display it
-ln -sf /proc/$$/fd/1 /var/log/salt/minion
-tail -f /var/log/salt/minion &
-
 # workaround for salt's service state
 echo -e '#!/bin/bash\necho "N 5"' > /sbin/runlevel
 chmod 775 /sbin/runlevel
@@ -16,11 +11,7 @@ chmod 775 /sbin/runlevel
 service salt-minion restart
 #debug may be useful for travis (-l debug)
 salt-call --local state.highstate saltenv=$1 pillarenv=one_user -l ${2-info} --no-color --out-file output
-#travis sometimes cannot handle properly this much of logs
-sleep 2
 echo "salt-call finished"
 cat output
-#seems that `cat output` can take some time...
-sleep 10
 echo "scanning output"
 awk '/^Failed:/ {if($2 != "0") exit 3}' output
