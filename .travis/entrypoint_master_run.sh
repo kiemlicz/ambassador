cced30a46dbcef46ddb58ede6267bfa425419f76
kiemlicz <stanislaw.dev@gmail.com>
2017-12-06 23:22:25 +0100

    moving installation to in-container process (as minion id was duplicated)

--- .travis/entrypoint_master_run.sh
+++ .travis/entrypoint_master_run.sh
@@ -3,6 +3,9 @@
 # use reactor to accept and start highstate on connected minions
 # wait for finish on master then invoke state.orchestrate
 
+curl -L https://bootstrap.saltstack.com > /tmp/bootstrap.sh
+sh /tmp/bootstrap.sh -M -N
+
 # workaround for
 # https://github.com/saltstack/salt/issues/37056
 rm -rf /var/run/salt/master/master_event_pub.ipc
@@ -11,4 +14,3 @@ rm -rf /var/run/salt/master/master_event_pull.ipc
 service salt-master restart
 
 /usr/bin/supervisord
-#/bin/bash
--- .travis/entrypoint_minion_run.sh
+++ .travis/entrypoint_minion_run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+curl -L https://bootstrap.saltstack.com | sh
+
 # workaround for salt's service state
 echo -e '#!/bin/bash\necho "N 5"' > /sbin/runlevel
 chmod 775 /sbin/runlevel
