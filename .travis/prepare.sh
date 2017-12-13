d3e23f747e06d54d55832367533503a804ba6a52
kiemlicz <stanislaw.dev@gmail.com>
2017-12-13 11:01:25 +0100

    refactor of dockerfiles layout

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -6,7 +6,17 @@ if [ -z "$DOCKER_IMAGE" ]; then
     exit 1
 fi
 
-DOCKER_COMPOSE_VERSION=1.17.1
+docker_compose_update() {
+    local docker_compose_version=1.17.1
+
+    docker-compose --version
+    sudo rm /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m` > docker-compose
+    chmod +x docker-compose
+    sudo mv docker-compose /usr/local/bin
+    docker-compose --version
+    docker --version
+}
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -15,23 +25,17 @@ sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
 
 case "$TEST_CASE" in
 salt-masterless-dry)
-    docker build -t "$DOCKER_IMAGE" -f .travis/"$DOCKER_IMAGE"/Dockerfile .
+    docker build -t "$DOCKER_IMAGE" -f .travis/"$DOCKER_IMAGE"/dry/Dockerfile .
     ;;
 salt-masterless-run)
-    docker build --build-arg=LOG_LEVEL="${LOG_LEVEL-info}" --build-arg=SALTENV="$SALTENV" -t "$DOCKER_IMAGE" -f .travis/"$DOCKER_IMAGE"/Dockerfile.run .
+    docker build --build-arg=LOG_LEVEL="${LOG_LEVEL-info}" --build-arg=SALTENV="$SALTENV" -t "$DOCKER_IMAGE" -f .travis/"$DOCKER_IMAGE"/masterless/Dockerfile .
     ;;
 salt-master-run)
-    docker-compose --version
-    sudo rm /usr/local/bin/docker-compose
-    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-    chmod +x docker-compose
-    sudo mv docker-compose /usr/local/bin
-    docker-compose --version
-    docker --version
+    docker_compose_update
     docker-compose -f .travis/"$DOCKER_IMAGE"/docker-compose.yml --project-directory=. up --no-start
 #    --build-arg LOG_LEVEL="${LOG_LEVEL-info}" --build-arg SALTENV="$SALTENV"
     ;;
 ambassador-run)
-    docker build --build-arg=FQDN="$TEST_FQDN" -t "$DOCKER_IMAGE" -f .travis/"$DOCKER_IMAGE"/Dockerfile.ambassador .
+    docker build --build-arg=FQDN="$TEST_FQDN" -t "$DOCKER_IMAGE" -f .travis/"$DOCKER_IMAGE"/ambassador/Dockerfile .
     ;;
 esac
