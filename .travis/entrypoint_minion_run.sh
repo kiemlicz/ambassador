0fdbcde96b10031430a10311760d668b56f5b449
kiemlicz <stanislaw.dev@gmail.com>
2018-02-10 13:39:12 +0100

    Refactoring supervisord starting and logging Working on getting docker-compose exit status that corresponds to one particular container

--- .travis/entrypoint_minion_run.sh
+++ .travis/entrypoint_minion_run.sh
@@ -3,12 +3,12 @@
 echo "Installing salt-minion, version: $@"
 
 curl -o /tmp/bootstrap.sh -L https://bootstrap.saltstack.com
-sh /tmp/bootstrap.sh -n stable ${1-""}
+sh /tmp/bootstrap.sh -n -X stable ${1-""}
 
 # workaround for salt's service state
 echo -e '#!/bin/bash\necho "N 5"' > /sbin/runlevel
 chmod 775 /sbin/runlevel
 
-#service salt-minion restart
+ln -sf /dev/stdout /var/log/salt/minion
 
 /usr/bin/supervisord
