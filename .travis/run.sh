6ca1049733a30c8101470b653cf8ba74b5d5aa48
kiemlicz <stanislaw.dev@gmail.com>
2018-02-11 11:30:07 +0100

    moving away container scanning logic (to travis.yml)

--- .travis/run.sh
+++ .travis/run.sh
@@ -8,10 +8,6 @@ salt-masterless-dry|salt-masterless-run)
 salt-master-run)
     # --exit-code-from master isn't the way to go as implies --abort-on-container-exit
     docker-compose -f .travis/"$DOCKER_IMAGE"/docker-compose.yml --project-directory=. --no-ansi up --no-build --no-recreate
-    rv=$(docker-compose -f .travis/"$DOCKER_IMAGE"/docker-compose.yml --project-directory=. ps -q | \
-        xargs docker inspect -f '{{ .State.ExitCode }}' | grep -v 0 | wc -l | tr -d ' ')
-    echo "inspected containers: $rv"
-    exit $rv
     ;;
 ambassador-run)
     docker run --privileged -h $TEST_FQDN "$DOCKER_IMAGE"
