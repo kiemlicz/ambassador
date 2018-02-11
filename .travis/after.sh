c78f1c7a8c7d9b1bce8ce365bcd7fde2f3f5693c
kiemlicz <stanislaw.dev@gmail.com>
2018-02-11 12:16:59 +0100

    moving away container scanning logic

---
+++ .travis/after.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+case "$TEST_CASE" in
+salt-master-run)
+    echo "scanning compose's containers"
+    exit $(docker-compose -f .travis/"$DOCKER_IMAGE"/docker-compose.yml --project-directory=. ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | grep -v 0 | wc -l | tr -d ' ')
+    ;;
+*)
+
+esac
