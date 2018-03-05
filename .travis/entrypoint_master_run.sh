cc954ed519fe265e741a5dc88cd975659f4fe288
kiemlicz <stanislaw.dev@gmail.com>
2018-03-05 16:15:55 +0100

    Syncing renderers for salt-master

--- .travis/entrypoint_master_run.sh
+++ .travis/entrypoint_master_run.sh
@@ -3,7 +3,7 @@
 # use reactor to accept and start highstate on connected minions
 # wait for finish on master then invoke state.orchestrate
 
-#salt-run saltutil.sync_all
+salt-run saltutil.sync_renderers
 /usr/bin/supervisord
 
 python /opt/scan_events.py /var/log/salt/events
