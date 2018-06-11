20bc4daa57db3f15b25fbbf35c7e0488c3a4fead
kiemlicz <stanislaw.dev@gmail.com>
2018-06-11 21:01:19 +0200

    salt config split into reusable parts

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
 
 
-# $1 tag name
+# $1 container name
 docker_push() {
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker commit $1 kiemlicz/ambassador:$1
-    docker push kiemlicz/ambassador:$1
+    if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
+        docker commit $1 kiemlicz/ambassador:$1
+        docker push kiemlicz/ambassador:$1
+
+    else
+        docker commit $1 kiemlicz/ambassador:"$1-failed"
+        docker push kiemlicz/ambassador:"$1-failed"
+    fi
 }
 
 case "$TEST_CASE" in
 salt-masterless-run)
-    if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
-        docker_push "salt-masterless-$TRAVIS_JOB_NUMBER"
-    else
-        docker_push "salt-masterless-$TRAVIS_JOB_NUMBER-failed"
-    fi
+    docker_push "salt-masterless-$TRAVIS_JOB_NUMBER"
     ;;
 salt-master-run)
     echo "salt-master-run publish is disabled"
     ;;
 ambassador-run)
-    if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
-        docker_push "ambassador-run-$TRAVIS_JOB_NUMBER"
-    else
-        docker_push "ambassador-run-$TRAVIS_JOB_NUMBER-failed"
-    fi
+    docker_push "ambassador-run-$TRAVIS_JOB_NUMBER"
     ;;
 esac
