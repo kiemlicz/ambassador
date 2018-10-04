3d59fe63bce4446a6db01f1139587e5b6035b8e4
kiemlicz <stanislaw.dev@gmail.com>
2018-10-04 23:11:48 +0200

    setup rewrite continued fixes, CIP computation on the container side redundant code removal

--- run.sh
+++ run.sh
@@ -139,7 +139,6 @@ readonly FOREMAN_CERT=$CERT
 readonly FOREMAN_KEY=$KEY
 readonly FOREMAN_PROXY_CERT=$PROXY_CERT
 readonly FOREMAN_PROXY_KEY=$PROXY_KEY
-echo "CA cert location: $CA_CERT, foreman cert: $FOREMAN_CERT"
 
 groupadd foreman
 chgrp foreman $FOREMAN_PROXY_KEY
