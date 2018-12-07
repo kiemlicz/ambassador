718040aa02fc837d6bbc13d980f61a17c2496707
stanislaw_jakiel <stanislaw.dev@gmail.com>
2018-12-07 12:02:58 +0100

    Trying k8s on travis

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -38,7 +38,7 @@ minikube_install() {
     chmod +x minikube
     sudo mv minikube /usr/local/bin/
     sudo minikube start --vm-driver=none
-    minikube update-context
+    sudo minikube update-context
     #wait until nodes report as ready
     JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
     until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
