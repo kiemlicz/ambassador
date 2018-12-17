b2bf7fdfe91f3e602f9bdfa2aa05737b7e0b31e2
kiemlicz <stanislaw.dev@gmail.com>
2018-12-17 18:27:27 +0100

    WIP kubernetes on travis, unified logging (first version)

--- .travis/run.sh
+++ .travis/run.sh
@@ -18,13 +18,18 @@ salt-master-run-compose)
     docker-compose -f .travis/docker-compose.yml --project-directory=. --no-ansi up --no-build --no-recreate
     ;;
 salt-master-run-k8s)
+    echo "Starting kubernetes deployment"
     trap k8s_log_error EXIT TERM INT
+    echo "trap setup complete"
     kubectl apply -f .travis/k8s-deployment.yaml
     # wait until salt-master and minion containers are running
     kubectl wait -n provisioning deployment/salt-master --for condition=available --timeout=120s
+    kubectl wait -n provisioning deployment/rsyslog --for condition=available --timeout=120s
     echo "Deployment ready:"
     kubectl get all -n provisioning
-    kubectl wait -n provisioning --for=delete deployment/salt-master --timeout=60m
+    logger=$(kubectl get pod -l app=rsyslog -n provisioning -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+    #kubectl wait -n provisioning --for=delete deployment/salt-master --timeout=60m
+    kubectl logs -n provisioning $logger -f
     echo "Deployment finished"
     ;;
 esac
