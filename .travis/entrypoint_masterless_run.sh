ac7226da377619f5b3ab7f146e0220a1593d6576
kiemlicz <stanislaw.dev@gmail.com>
2018-12-18 15:40:57 +0100

    WIP kubernetes on travis

--- .travis/entrypoint_masterless_run.sh
+++ .travis/entrypoint_masterless_run.sh
@@ -7,7 +7,7 @@ echo "starting (args = $@)"
 # workaround for salt's service state
 #echo -e '#!/bin/bash\necho "N 5"' > /sbin/runlevel
 #chmod 775 /sbin/runlevel
-#service salt-minion restart
+service salt-minion restart
 #debug may be useful for travis (-l debug)
 salt-call --local state.highstate saltenv=$1 pillarenv=$2 -l ${3-info} --no-color --out-file output
 salt_call_ret_val=$?
