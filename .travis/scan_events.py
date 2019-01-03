d104b998f5dd975e5db8d061c2f9ca45bf849f70
kiemlicz <stanislaw.dev@gmail.com>
2019-01-03 17:28:54 +0100

    WIP kubernetes on travis, logging events

--- .travis/scan_events.py
+++ .travis/scan_events.py
@@ -3,10 +3,13 @@ import sys
 import json
 import re
 import argparse
+import logging
 
 
 JOB_RET = re.compile("salt/job/\d+/ret/\S+")
 RUN_RET = re.compile("salt/run/\d+/ret")
+
+logging.basicConfig(filename='/var/log/salt/scan', level=logging.DEBUG)
 
 parser = argparse.ArgumentParser(description='Scans event dump file.')
 parser.add_argument('events', type=str, help='event list file')
@@ -16,8 +19,8 @@ args = parser.parse_args()
 def _validate_jobs(event_list):
     failures = [e for e in event_list if not e["success"] or e["success"] and e["retcode"] != 0]
     for f in failures:
-        print("Job: {}, on minion: {}, finished with error:\n{}\n"
-              .format(f["fun"], f["id"], json.dumps(f["return"], indent=4)).decode('string_escape'), file=sys.stderr)
+        logging.error("Job: {}, on minion: {}, finished with error:\n{}\n"
+              .format(f["fun"], f["id"], json.dumps(f["return"], indent=4)).decode('string_escape'))
 
     return len(failures) == 0
 
@@ -25,10 +28,13 @@ def _validate_jobs(event_list):
 def _validate_runs(event_list):
     failures = [e for e in event_list if not e["success"]]
     for f in failures:
-        print("Run: {}, from master, finished with error:\n{}\n"
-              .format(f["fun"], json.dumps(f["return"], indent=4)).decode('string_escape'), file=sys.stderr)
+        logging.error("Run: {}, from master, finished with error:\n{}\n"
+              .format(f["fun"], json.dumps(f["return"], indent=4)).decode('string_escape'))
 
     return len(failures) == 0
+
+
+logging.info("Scanning events")
 
 
 with open(args.events) as f:
@@ -41,7 +47,6 @@ runs = [e["data"] for e in all_events if RUN_RET.match(e["tag"])]
 jobs_ok = _validate_jobs(jobs)
 runs_ok = _validate_runs(runs)
 
+logging.info("Event count: {}".format(len(all_events)))
 if not jobs_ok or not runs_ok:
     sys.exit(3)
-else:
-    print("Event count: {}".format(len(all_events)))
