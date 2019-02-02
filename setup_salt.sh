5404288ee27f13c966de2f15d9898865e9fe20a6
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-02-02 19:20:43 +0100

    using backports as pygit2==0.27.3 fixes lib incompatibility

--- setup_salt.sh
+++ setup_salt.sh
@@ -16,12 +16,10 @@ echo "$SALTSTACK_REPO_ENTRY" | tee /etc/apt/sources.list.d/saltstack.list
 echo "$BACKPORTS_REPO" | tee /etc/apt/sources.list.d/backports.list
 
 apt-get update
-#0.27.4 is not yet supported in salt
-#apt-get install -t stretch-backports -y libgit2-dev
-curl -L https://gist.githubusercontent.com/kiemlicz/33e891dd78e985bd080b85afa24f5d0a/raw/b9aba40aa30f238a24fe4ecb4ddc1650d9d685af/init.sh | bash
-apt-get install -y salt-master salt-api salt-ssh tcpdump nano vim
-
+apt-get install -t stretch-backports -y libgit2-dev
 #old 'compilation' process can be found here: https://gist.github.com/kiemlicz/33e891dd78e985bd080b85afa24f5d0a
+#curl -L https://gist.githubusercontent.com/kiemlicz/33e891dd78e985bd080b85afa24f5d0a/raw/b9aba40aa30f238a24fe4ecb4ddc1650d9d685af/init.sh | bash
+apt-get install -y salt-master salt-api salt-ssh tcpdump nano vim
 
 #somehow these dependencies are already present, that's why use of --upgrade
 #as long as this is not released https://github.com/saltstack/salt/issues/44601 CherryPy max supported version is 11.2.0
