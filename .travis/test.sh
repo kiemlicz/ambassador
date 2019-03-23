ccf00f8aa8b7c151574fa5132708e9742c82b506
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-03-23 22:30:52 +0100

    Less logs

--- .travis/test.sh
+++ .travis/test.sh
@@ -34,15 +34,16 @@ salt-master-run-k8s)
     echo "Deployment ready:"
     kubectl get all -n salt-provisioning
 
-    while sleep 5m; do echo -e "\nEvents:$(kubectl get events --all-namespaces)\nStatus:$(kubectl get all --all-namespaces)"; done &
+    #while sleep 5m; do echo -e "\nEvents:$(kubectl get events --all-namespaces)\nStatus:$(kubectl get all --all-namespaces)"; done &
 
-    echo "Should accept new minion"
+    echo -e "\nShould accept new minion\n"
     kubectl -n salt-provisioning delete pod -l name=salt-minion
     kubectl -n salt-provisioning wait pod -l name=salt-minion --for condition=ready --timeout=5m
 
-    echo "Should work after master crash"
+    echo -e "\nShould work after master crash\n"
     kubectl -n salt-provisioning delete pod -l name=salt-master
     kubectl -n salt-provisioning wait pod -l name=salt-master --for condition=ready --timeout=5m
+    kubectl -n salt-provisioning exec -it $(kubectl -n salt-provisioning get pod -l name=salt-master -o jsonpath='{.items[0].metadata.name}') -- salt-key -L
     kubectl -n salt-provisioning exec -it $(kubectl -n salt-provisioning get pod -l name=salt-master -o jsonpath='{.items[0].metadata.name}') -- salt '*' test.ping | grep True
 
     echo "Deployment finished"
