4be5a7b3b066a899df1b14091469aef0d18e4515
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-05-10 23:12:44 +0200

    devtool state refactor

--- salt/base/_states/devtool.py
+++ salt/base/_states/devtool.py
@@ -11,23 +11,6 @@ def managed(name, download_url, destination_dir, user, group, enforce_toplevel=T
     ret = {'name': name, 'changes': {}, 'result': False, 'comment': ''}
     log = logging.getLogger(__name__)
 
-    def _format_comments(comments):
-        '''
-        Return a joined list
-        '''
-        ret = '. '.join(comments)
-        if len(comments) > 1:
-            ret += '.'
-        return ret
-
-    def _fail(ret, msg, comments=None):
-        ret['result'] = False
-        if comments:
-            msg += '\n\nFailure reason: '
-            msg += _format_comments(comments)
-        ret['comment'] = msg
-        return ret
-
     # listing before extraction as archive.extracted has bug and requires to pass enforce_ownership_on:
     # https://github.com/saltstack/salt/issues/38605
     try:
@@ -41,10 +24,12 @@ def managed(name, download_url, destination_dir, user, group, enforce_toplevel=T
             log.error("Cache clear result: {}".format(result))
         else:
             log.error("Unable to clear cache, bogus cached file: {}".format(location))
-        raise e
+        return __utils__['common.fail'](ret, "DEVTOOL: failed to list archive ({}), error:".format(download_url, str(e)))
+
     extract_dir = os.path.commonprefix(archive_contents)  # relative path
     if not extract_dir and not enforce_toplevel:
-        return _fail(ret, "Cannot find root directory in extracted archive, try setting enforce_toplevel: True", archive_contents)
+        return __utils__['common.fail'](ret, "DEVTOOL: Cannot find root directory in extracted archive, try setting enforce_toplevel: True", archive_contents)
+
     extract_location = os.path.join(destination_dir, extract_dir)
     log.debug("Will extract to: {0}".format(extract_location))
 
@@ -54,7 +39,7 @@ def managed(name, download_url, destination_dir, user, group, enforce_toplevel=T
     extract_result = __states__['archive.extracted'](name=destination_dir, source=download_url, user=user, group=group,
                                                      enforce_ownership_on=extract_location, skip_verify=True, trim_output=50)
     if not extract_result['result']:
-        return _fail(ret, "Cannot extract archive from: {0}".format(download_url), [extract_result['comment']])
+        return __utils__['common.fail'](ret, "DEVTOOL: Cannot extract archive from: {0}".format(download_url), [extract_result['comment']])
 
     old_state = ["{0} previously didn't exist".format(download_url)]
     if not extract_result['changes']:
@@ -63,12 +48,10 @@ def managed(name, download_url, destination_dir, user, group, enforce_toplevel=T
 
     symlink_result = __states__['file.symlink'](name=name, target=extract_location, user=user)
     if not symlink_result['result']:
-        return _fail(ret, "Cannot create symlink ({0}) to: {1}".format(name, extract_location))
+        return __utils__['common.fail'](ret, "DEVTOOL: Cannot create symlink ({0}) to: {1}".format(name, extract_location))
 
-    ret['comment'] = "Success"
     ret['changes'].update({'devtool': {
         'old': old_state,
         'new': ["Extracted to: {0}".format(extract_location)]
     }})
-    ret['result'] = True
-    return ret
+    return __utils__['common.success'](ret, "DEVTOOL: success")
