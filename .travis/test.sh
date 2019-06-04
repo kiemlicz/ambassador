ede3a087547ecbe4b5c670fb47c2b4a598be98c6
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-06-04 22:12:45 +0200

    Proper return code assert in trap

--- .travis/test.sh
+++ .travis/test.sh
@@ -5,16 +5,24 @@ set -e
 source .travis/common.sh
 
 k8s_log_error() {
-    echo -e "\n####################\n\nERROR DURING KUBERNETES DEPLOYMENT\n$(date)\n####################\n"
-    kubectl get all --all-namespaces
-    echo -e "\n[ERROR]Events:"
-    kubectl get events --all-namespaces
-    echo -e "\n[ERROR] Pods info"
-    kubectl -n salt-provisioning describe pods
-    echo -e "\n[ERROR] Master logs"
-    kubectl -n salt-provisioning logs -l name=salt-master
-    echo -e "\n[ERROR] Minion logs"
-    kubectl -n salt-provisioning logs -l name=salt-minion
+    local rv=$?
+    case $rv in
+    0)
+        echo "no error"
+    ;;
+    *)
+        echo -e "\n####################\n\nERROR DURING KUBERNETES DEPLOYMENT\n$(date)\n####################\n"
+        kubectl get all --all-namespaces
+        echo -e "\n[ERROR]Events:"
+        kubectl get events --all-namespaces
+        echo -e "\n[ERROR] Pods info"
+        kubectl -n salt-provisioning describe pods
+        echo -e "\n[ERROR] Master logs"
+        kubectl -n salt-provisioning logs -l name=salt-master
+        echo -e "\n[ERROR] Minion logs"
+        kubectl -n salt-provisioning logs -l name=salt-minion
+        ;;
+    esac
 }
 
 case "$1" in
@@ -59,31 +67,31 @@ salt-master-run-k8s)
     # wait for logger first, not sure if --wait waits for dependencies
     #kubectl wait -n salt-provisioning pod -l app=logstash --for condition=ready --timeout=5m
     logger=$(kubectl get pod -l app=logstash -n salt-provisioning -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-    echo -e "\nlogs from: $logger"
+    echo -e "starting logs from: $logger"
     kubectl -n salt-provisioning logs -f $logger &
-
-    echo "Deployment ready:"
-    kubectl get all -n salt-provisioning
 
     #while sleep 5m; do echo -e "\nEvents:$(kubectl get events --all-namespaces)\nStatus:$(kubectl get all --all-namespaces)"; done &
 
-    echo -e "\nShould accept new minion\n"
+    echo -e "\nTest 1: should accept new minion\n"
     kubectl -n salt-provisioning delete pod -l name=salt-minion
     kubectl -n salt-provisioning wait pod -l name=salt-minion --for condition=ready --timeout=5m
 # this will still contain old minion that will fail the ping
 #    echo "Pinging minions"
 #    kubectl -n salt-provisioning exec -it $(kubectl -n salt-provisioning get pod -l name=salt-master -o jsonpath='{.items[0].metadata.name}') -- salt '*' test.ping
 
-    echo -e "\nShould work after master crash\n"
+    echo -e "\nTest 2: should work after master crash\n"
     kubectl -n salt-provisioning delete pod -l name=salt-master
     kubectl -n salt-provisioning wait pod -l name=salt-master --for condition=ready --timeout=5m
 
     # the minion must first re-auth to master that got down, will do that after auth_timeout (?) + thorium re-scan
     # it will finally clean the old keys but honestly, why does it take so long?
     sleep 180
-    echo "\nAfter 3 min sleep: listing who's up"
-    kubectl -n salt-provisioning exec -it $(kubectl -n salt-provisioning get pod -l name=salt-master -o jsonpath='{.items[0].metadata.name}') -- salt-key -L
-    kubectl -n salt-provisioning exec -it $(kubectl -n salt-provisioning get pod -l name=salt-master -o jsonpath='{.items[0].metadata.name}') -- salt-run manage.up
+    echo -e "\nAfter 3 min sleep: listing who's up"
+    master=$(kubectl -n salt-provisioning get pod -l name=salt-master -o jsonpath='{.items[0].metadata.name}')
+    kubectl -n salt-provisioning exec -it $master -- salt-key -L
+    kubectl -n salt-provisioning exec -it $master -- salt-run manage.up
+    echo -e "\nSalt-master POD logs:\n"
+    kubectl -n salt-provisioning logs $master
 # fixme
 # this will still contain old minion that will fail the ping
 #    echo "Pinging minions"
--- .travis/thorium/minion/init.sls
+++ .travis/thorium/minion/init.sls
@@ -3,6 +3,6 @@ statreg:
 
 keydel:
   key.timeout:
-    - delete: 60
+    - delete: 30
     - require:
       - status: statreg
