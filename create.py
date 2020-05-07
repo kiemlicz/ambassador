b2727aa53a9844fe18b6f87bcecd60c3dc32bdf2
stanislaw_jakiel <stanislaw.dev@gmail.com>
2020-05-07 19:37:48 +0200

    wip install process configurable for lxc or not

--- create.py
+++ create.py
@@ -1,20 +1,21 @@
-import lxc
-import sys
-import os
 import argparse
-import logging
-import yaml
 import datetime
+import logging
+import os
 from distutils import dir_util
-from shutil import copyfile
 from pathlib import Path
+from shutil import copyfile
+import urllib.request
+import sys
+import yaml
 from pykeepass import PyKeePass
-
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
+
 parser = argparse.ArgumentParser(description='Setup LXC container and provision with Salt')
-parser.add_argument('--name', help="provide container name", required=True)
+parser.add_argument('--lxc', help="install within LXC container", required=True, action='store_true')
+parser.add_argument('--name', help="provide container name", required=False)
 parser.add_argument('--ifc', help="provide interface to attach container to", required=True)
 parser.add_argument('--configs', help="provide Salt Minion config", required=True, nargs='+', type=str)
 parser.add_argument('--directories', help="provide directories to copy to container (/srv)", required=False, nargs='+', type=str, default=["salt"])
@@ -27,88 +28,113 @@ parser.add_argument('--rootfs', help="provide container rootfs path", required=F
 parser.add_argument('--autostart', help="should the container autostart", required=False, default=True, action='store_true')
 args = parser.parse_args()
 
-start_time = datetime.datetime.now()
-container_name = args.name
-c = lxc.Container(name=container_name)
+lxc = args.lxc
+prereq_url = "https://gist.githubusercontent.com/kiemlicz/1aa8c2840f873b10ecd744bf54dcd018/raw/1fb26207f7d9665989fc7019b1c0ac919383331a/setup_salt_requisites.sh"
+bootstrap_url = "https://bootstrap.saltstack.com"
 
-if not c.defined:
-    # two interfaces?
-    c.set_config_item("lxc.net.0.type", "veth")
-    c.set_config_item("lxc.net.0.link", args.ifc)
-    c.set_config_item("lxc.net.0.flags", "up")
-    c.set_config_item("lxc.apparmor.profile", "unconfined")  # todo otherwise apache2 won't start, find proper solution
 
-    if args.autostart:
-        c.set_config_item("lxc.start.auto", "1")
-    # todo increase size
-    if not c.create(template="debian", flags=0, args={"release": "buster", "arch": "amd64"}):
-        log.error("Unable to create LXC container")
-        sys.exit(3)
+def ensure_container(container_name):
+    c = lxc.Container(name=container_name)
+    if not c.defined:
+        # two interfaces?
+        c.set_config_item("lxc.net.0.type", "veth")
+        c.set_config_item("lxc.net.0.link", args.ifc)
+        c.set_config_item("lxc.net.0.flags", "up")
+        c.set_config_item("lxc.apparmor.profile",
+                          "unconfined")  # todo otherwise apache2 won't start, find proper solution
 
-if not c.running:
-    log.info("Starting %s", container_name)
-    if not c.start():
-        log.error("Unable to start %s, check output of command: lxc-start -n %s -F", container_name, container_name)
-        sys.exit(3)
-    if not c.get_ips(timeout=60):
-        log.error("Unable to start LXC container")
-        sys.exit(3)
+        if args.autostart:
+            c.set_config_item("lxc.start.auto", "1")
+        # todo increase size
+        if not c.create(template="debian", flags=0, args={"release": "buster", "arch": "amd64"}):
+            log.error("Unable to create LXC container")
+            sys.exit(3)
+    if not c.running:
+        log.info("Starting %s", container_name)
+        if not c.start():
+            log.error("Unable to start %s, check output of command: lxc-start -n %s -F", container_name, container_name)
+            sys.exit(3)
+        if not c.get_ips(timeout=60):
+            log.error("Unable to start LXC container")
+            sys.exit(3)
+    return c
 
-log.info("Inserting files")
-container_rootfs = os.path.join(args.rootfs, container_name, "rootfs")
-for dir in args.directories:
-    log.info("Copying directory: %s", dir)
-    dir_util.copy_tree(dir, os.path.join(container_rootfs, "srv", dir))
 
-if args.top:
-    l = args.top_location
-    if l.startswith("/"):
-        l = l[1:]
-    dest = os.path.join(container_rootfs, l, "top.sls")
-    log.info("Copying: %s, into: %s", args.top, dest)
-    copyfile(args.top, dest)
+def populate_files(rootfs):
+    log.info("Inserting files")
+    for dir in args.directories:
+        log.info("Copying directory: %s", dir)
+        dir_util.copy_tree(dir, os.path.join(rootfs, "srv", dir))
 
-kdbx_salt_config = {'kdbx': {'driver': 'kdbx'}}
-if args.kdbx_key:
-    Path(os.path.join(container_rootfs, "etc", "salt", "keys")).mkdir(parents=True, exist_ok=True)
-    key_filename = os.path.basename(args.kdbx_key)
-    copyfile(args.kdbx_key, os.path.join(container_rootfs, "etc", "salt", "keys", key_filename))
-    kdbx_salt_config['kdbx']['key_file'] = os.path.join(os.sep, "etc", "salt", "keys", key_filename)
-if args.kdbx_pass:
-    kdbx_salt_config['kdbx']['password'] = args.kdbx_pass
-if args.kdbx:
-    Path(os.path.join(container_rootfs, "etc", "salt", "kdbx")).mkdir(parents=True, exist_ok=True)
-    kdbx_filename = os.path.basename(args.kdbx)
-    copyfile(args.kdbx, os.path.join(container_rootfs, "etc", "salt", "kdbx", kdbx_filename))
-    kdbx_salt_config['kdbx']['db_file'] = os.path.join(os.sep, "etc", "salt", "kdbx", kdbx_filename)
-    kdbx_config = os.path.join(container_rootfs, "etc", "salt", "minion.d", "{}-kdbx.conf".format(container_name))
-    Path(os.path.dirname(kdbx_config)).mkdir(parents=True, exist_ok=True)
-    with open(kdbx_config, 'w') as minion_config_file:
-        yaml.dump(kdbx_salt_config, minion_config_file)
-    log.info("Inserting secrets")
-    kdbx = PyKeePass(args.kdbx, args.kdbx_pass, args.kdbx_key)  # fixme how will it behave if no key
-    entry = kdbx.find_entries_by_path(container_name, first=True)
-    if entry and entry.attachments:
-        for attachment in entry.attachments:
-            with open(os.path.join(container_rootfs, "etc", "salt", "keys", attachment.filename), 'wb') as a:
-                a.write(attachment.data)
-    else:
-        log.warning("No secrets for: %s found", container_name)
+    if args.top:
+        l = args.top_location
+        if l.startswith("/"):
+            l = l[1:]
+        dest = os.path.join(rootfs, l, "top.sls")
+        log.info("Copying: %s, into: %s", args.top, dest)
+        copyfile(args.top, dest)
 
-if args.configs:
     for config in args.configs:
         log.info("Copying Salt config: %s", config)
         # todo check without basename
-        copyfile(config, os.path.join(container_rootfs, "etc", "salt", "minion.d", os.path.basename(config)))
+        copyfile(config, os.path.join(rootfs, "etc", "salt", "minion.d", os.path.basename(config)))
 
-log.info("Running inside: %s", container_name)
-c.attach_wait(lxc.attach_run_command, ["apt-get", "install", "-y", "curl"])
-c.attach_wait(lxc.attach_run_command, ["curl", "-o", "/tmp/pre.sh", "-L", "https://gist.githubusercontent.com/kiemlicz/1aa8c2840f873b10ecd744bf54dcd018/raw/1fb26207f7d9665989fc7019b1c0ac919383331a/setup_salt_requisites.sh"])
-c.attach_wait(lxc.attach_run_command, ["curl", "-o", "/tmp/bootstrap-salt.sh", "-L", "https://bootstrap.saltstack.com"])
-c.attach_wait(lxc.attach_run_command, ["bash", "/tmp/pre.sh"])
-c.attach_wait(lxc.attach_run_command, ["bash", "/tmp/bootstrap-salt.sh", "-x", "python3"])
-c.attach_wait(lxc.attach_run_command, ["gpg", "--homedir", "/etc/salt/gpgkeys", "--import", "/etc/salt/keys/pillargpg.gpg"])
-c.attach_wait(lxc.attach_run_command, ["salt-call", "--local", "saltutil.sync_all"])
-c.attach_wait(lxc.attach_run_command, ["salt-call", "--local", "state.highstate"])
+    kdbx_salt_config = {'kdbx': {'driver': 'kdbx'}}
+    if args.kdbx_key:
+        Path(os.path.join(rootfs, "etc", "salt", "keys")).mkdir(parents=True, exist_ok=True)
+        key_filename = os.path.basename(args.kdbx_key)
+        copyfile(args.kdbx_key, os.path.join(rootfs, "etc", "salt", "keys", key_filename))
+        kdbx_salt_config['kdbx']['key_file'] = os.path.join(os.sep, "etc", "salt", "keys", key_filename)
+    if args.kdbx_pass:
+        kdbx_salt_config['kdbx']['password'] = args.kdbx_pass
+    if args.kdbx:
+        Path(os.path.join(rootfs, "etc", "salt", "kdbx")).mkdir(parents=True, exist_ok=True)
+        kdbx_filename = os.path.basename(args.kdbx)
+        copyfile(args.kdbx, os.path.join(rootfs, "etc", "salt", "kdbx", kdbx_filename))
+        kdbx_salt_config['kdbx']['db_file'] = os.path.join(os.sep, "etc", "salt", "kdbx", kdbx_filename)
+        kdbx_config = os.path.join(rootfs, "etc", "salt", "minion.d", "{}-kdbx.conf".format(container_name))
+        Path(os.path.dirname(kdbx_config)).mkdir(parents=True, exist_ok=True)
+        with open(kdbx_config, 'w') as minion_config_file:
+            yaml.dump(kdbx_salt_config, minion_config_file)
+        log.info("Inserting secrets")
+        kdbx = PyKeePass(args.kdbx, args.kdbx_pass, args.kdbx_key)  # fixme how will it behave if no key
+        entry = kdbx.find_entries_by_path(container_name, first=True)
+        if entry and entry.attachments:
+            for attachment in entry.attachments:
+                with open(os.path.join(rootfs, "etc", "salt", "keys", attachment.filename), 'wb') as a:
+                    a.write(attachment.data)
+        else:
+            log.warning("No secrets for: %s found", container_name)
+
+
+def install():
+    response = urllib.request.urlopen(prereq_url)
+    prereq_script = response.read().decode('utf-8')
+    response = urllib.request.urlopen(bootstrap_url)
+    bootstrap_script = response.read().decode('utf-8')
+    with open("/tmp/prereq.sh", "w+") as prereq, open("/tmp/bootstrap-salt.sh", "w+") as bootstrap:
+        prereq.write(prereq_script)
+        bootstrap.write(bootstrap_script)
+    os.chmod("/tmp/prereq.sh", 0o755)
+    os.chmod("/tmp/bootstrap-salt.sh", 0o755)
+    os.system("/tmp/prereq.sh")
+    os.system("/tmp/bootstrap-salt.sh -x python3")
+    os.system("gpg --homedir /etc/salt/gpgkeys --import /etc/salt/keys/pillargpg.gpg")
+    os.system("salt-call --local saltutil.sync_all")
+    os.system("salt-call --local state.highstate")
+
+
+start_time = datetime.datetime.now()
+
+if lxc:
+    container_name = args.name
+    c = ensure_container(container_name)
+    populate_files(os.path.join(args.rootfs, container_name, "rootfs"))
+    log.info("Running inside: %s", container_name)
+    c.attach_wait(install)
+else:
+    populate_files(os.sep)
+    install()
+
 end_time = datetime.datetime.now()
 log.info("Completed: %s", end_time - start_time)
