8900ef1e0fdf6d9239dd53945b5244faaf4d4878
stanislaw_jakiel <stanislaw.dev@gmail.com>
2021-01-03 12:43:30 +0100

    pip-based salt installation, adjusting paths

--- .travis/entrypoint.sh
+++ .travis/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [ $API_ENABLED = true ]; then
-  /usr/bin/salt-api &
-  /usr/bin/salt-master &
+  /usr/local/bin/salt-api &
+  /usr/local/bin/salt-master &
   wait -n
 else
   # replace the bash process with salt-master
-  exec /usr/bin/salt-master
+  exec /usr/local/bin/salt-master
 fi
