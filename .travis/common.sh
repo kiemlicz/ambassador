1139835ab815b0372b03dc511868afe14f39e8f0
stanislaw_jakiel <stanislaw.dev@gmail.com>
2021-04-11 19:26:41 +0200

    temp workaround in salt_install function (will be replaced by install.py)

--- .travis/common.sh
+++ .travis/common.sh
@@ -62,7 +62,7 @@ docker_push() {
 
 salt_install() {
     sudo apt-get update && sudo apt-get install -y curl python3-setuptools python3-pip rustc
-    sudo pip3 install setuptools-rust~=0.12.1
+    sudo pip3 install pip~=21.0.1 setuptools-rust~=0.12.1
     sudo pip3 install -r $TRAVIS_BUILD_DIR/config/requirements.txt
     sudo mkdir -p /etc/salt/minion.d/
     sudo cp ${1-".travis/config/masterless.conf"} /etc/salt/minion.d/
